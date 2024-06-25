719194378f951a3ce3500de51ea583ae300fe3fe
Gabriel Arthur <dev.gabrielfiusa@gmail.com>
2024-06-25 00:04:35 -0300

    feat: adding player

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -8,10 +8,13 @@ import 'package:pixel_adventure/levels/level.dart';
 class PixelAdventure extends FlameGame {
   late final CameraComponent cam;
 
-  final world = Level();
+  final world = Level(levelName: 'Level-02');
 
   @override
-  FutureOr<void> onLoad() {
+  FutureOr<void> onLoad() async {
+    //Loading all images into cache
+    await images.loadAllImages();
+
     cam = CameraComponent.withFixedResolution(
       world: world,
       width: 640,
